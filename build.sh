2408c767c6bf4a7d4a3c3b2f92286ba98a2686af
Larry Zhao <larry.zhao01@sap.com>
2019-06-13 11:01:26 +0800

    rebuild: ae4bcd56d68cce2804fd238d11f5481ac561e7f0 -> 43b163baa67bffe49aeab68e0245ffb7935a1903

--- build.sh
+++ build.sh
@@ -11,8 +11,13 @@ if [ ! -d ${MASTER_WORKTREE} ]; then
 	git worktree add -f ${MASTER_WORKTREE} master
 fi
 
-MASTER_COMMIT=$(cd ${MASTER_WORKTREE} && git pull > /dev/null && git rev-parse HEAD)
+MASTER_COMMIT=$(cd ${MASTER_WORKTREE} && git pull ${REPO}> /dev/null && git rev-parse HEAD)
 LAST_BUILD_COMMIT=$(cat BUILD.sha1)
+if [ ${MASTER_COMMIT} == ${LAST_BUILD_COMMIT} ]; then 
+	echo "no more commit"
+	exit 0
+fi
+
 swgen -input ${MASTER_WORKTREE} -output $PWD -verbose -root /diary -template ${MASTER_WORKTREE}/_template/page.html
 echo ${MASTER_COMMIT} > BUILD.sha1
 git add --all
